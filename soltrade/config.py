1bbd7f6851c235fac01eaea08804cd9e019e60a1
EtcherFX <96310503+etcherfx@users.noreply.github.com>
2024-09-29 17:33:47 -0700

    feat: Redo configuration system
    Signed-off-by: Etcher <96310503+etcherfx@users.noreply.github.com>

--- soltrade/config.py
+++ soltrade/config.py
@@ -7,15 +7,14 @@ from solders.keypair import Keypair
 from solana.rpc.api import Client
 from soltrade.log import log_general
 from dotenv import load_dotenv
-import os
+
 
 class Config:
-    def __init__(self):
+    def __init__(self, path):
         load_dotenv()
-
         self.api_key = None
         self.private_key = None
-        self.custom_rpc_https = None
+        self.rpc_https = None
         self.primary_mint = None
         self.primary_mint_symbol = None
         self.sol_mint = "So11111111111111111111111111111111111111112"
@@ -25,37 +24,41 @@ class Config:
         self.trading_interval_minutes = None
         self.slippage = None  # BPS
         self.computeUnitPriceMicroLamports = None
-        self.stoploss = None
-        self.trailing_stoploss = None
-        self.trailing_stoploss_target = None
         self.telegram = None
         self.tg_bot_token = None
         self.tg_bot_uid = None
+        self.path = os.path.join(os.path.dirname(path), "config.json")
         self.load_config()
 
     def load_config(self):
-        self.api_key = os.getenv('API_KEY')
+        self.api_key = os.getenv("API_KEY")
         self.private_key = os.getenv("WALLET_PRIVATE_KEY")
-        self.custom_rpc_https = os.getenv("custom_rpc_https", "https://api.mainnet-beta.solana.com/")
-        self.primary_mint = os.getenv("PRIMARY_MINT", "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v")
-        self.primary_mint_symbol = os.getenv("PRIMARY_MINT_SYMBOL", "USD")
-        self.secondary_mint = os.getenv("SECONDARY_MINT", "")
-        self.secondary_mint_symbol = os.getenv("SECONDARY_MINT_SYMBOL", "UNKNOWN")
-        self.price_update_seconds = int(os.getenv("PRICE_UPDATE_SECONDS") or 60)
-        self.trading_interval_minutes = int(os.getenv("TRADING_INTERVALS_MINUTE") or 1)
-        self.slippage = int(os.getenv("SLIPPAGE") or 50)
 
-        # DEFAULT FEE OF ROUGHLY $0.04 TODAY
-        self.computeUnitPriceMicroLamports = int(os.getenv("COMPUTE_UNIT_PRICE_MICRO_LAMPORTS") or 20 * 14000)
-        
-        #with open(self.path, 'r') as file:
-        #    try:
-        #        config_data = json.load(file)
-        #        self.verbose = config_data.get("verbose", False)
-        #        self.strategy = config_data.get("strategy", "default")
-        #        self.stoploss = config_data["stoploss"]
-        #        self.trailing_stoploss = config_data["trailing_stoploss"]
-        #        self.trailing_stoploss_target = config_data["trailing_stoploss_target"]
+        default_config = {
+            "rpc_https": "https://api.mainnet-beta.solana.com/",
+            "primary_mint": "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+            "primary_mint_symbol": "USDC",
+            "secondary_mint": "So11111111111111111111111111111111111111112",
+            "secondary_mint_symbol": "SOL",
+            "price_update_seconds": 30,
+            "trading_interval_minutes": 0.5,
+            "slippage": 50,
+            "telegram": None,
+            "tg_bot_token": None,
+            "tg_bot_uid": None,
+            "verbose": True,
+            "strategy": "default",
+        }
+
+        with open(self.path, "r") as file:
+            try:
+                config_data = json.load(file)
+                for key, default_value in default_config.items():
+                    setattr(self, key, config_data.get(key, default_value))
+                    if not getattr(self, key):
+                        setattr(self, key, default_value)
+            except json.JSONDecodeError as e:
+                print(f"Error loading config: {e}")
 
     @property
     def keypair(self) -> Keypair:
@@ -80,9 +83,13 @@ class Config:
 
     @property
     def decimals(self) -> int:
-        response = self.client.get_account_info_json_parsed(Pubkey.from_string(config().secondary_mint)).to_json()
+        response = self.client.get_account_info_json_parsed(
+            Pubkey.from_string(config().secondary_mint)
+        ).to_json()
         json_response = json.loads(response)
-        value = 10 ** json_response["result"]["value"]["data"]["parsed"]["info"]["decimals"]
+        value = (
+            10 ** json_response["result"]["value"]["data"]["parsed"]["info"]["decimals"]
+        )
         return value
 
 
