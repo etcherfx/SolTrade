ad5d6001fc8d774d7436865057ddf3ea1d13c991
EtcherFX <96310503+etcherfx@users.noreply.github.com>
2024-10-03 17:32:01 -0700

    chore: Use csv instead of json
    Signed-off-by: Etcher <96310503+etcherfx@users.noreply.github.com>

--- soltrade/trading.py
+++ soltrade/trading.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+from io import StringIO
 
 import pandas as pd
 import requests
@@ -40,9 +41,8 @@ def fetch_candlestick() -> dict:
 def perform_analysis():
     log_general.debug("Soltrade is analyzing the market; no trade has been executed.")
 
-    global stoploss, takeprofit, trailing_stoploss, entry_price
-
-    market().load_position()
+    market_instance = market()
+    market_instance.load_position()
 
     # Converts JSON response for DataFrame manipulation
     candle_json = fetch_candlestick()
@@ -54,78 +54,76 @@ def perform_analysis():
     df["time"] = pd.to_datetime(df["time"], unit="s")
     cl = df["close"]
     df = strategy(df)
-    json_file_path = "data.json"
+    data_file_path = "data.csv"
     print(df.tail(2))
 
-    for col in [
-        "entry_price",
-        "stoploss",
-        "trailing_stoploss",
-        "trailing_stoploss_target",
-    ]:
-        if col not in df.columns:
-            df[col] = None
+    config_instance = config()
+    primary_mint = config_instance.primary_mint
+    primary_mint_symbol = config_instance.primary_mint_symbol
+    secondary_mint = config_instance.secondary_mint
+    secondary_mint_symbol = config_instance.secondary_mint_symbol
 
-    if not market().position:
-        input_amount = find_balance(config().primary_mint)
+    if not market_instance.position:
+        input_amount = find_balance(primary_mint)
         if df["entry"].iloc[-1] == 1:
-            buy_msg = f"Soltrade has detected a buy signal using {input_amount} ${config().primary_mint_symbol}."
-            log_transaction.info(buy_msg)
-            # log_transaction.info(get_statistics())
             if input_amount <= 0:
-                fund_msg = f"Soltrade has detected a buy signal, but does not have enough ${config().primary_mint_symbol} to trade."
-                log_transaction.info(fund_msg)
+                log_transaction.info(
+                    f"Soltrade has detected a buy signal, but does not have enough ${primary_mint_symbol} to trade."
+                )
                 return
-            is_swapped = asyncio.run(perform_swap(input_amount, config().primary_mint))
+            log_transaction.info(
+                f"Soltrade has detected a buy signal using {input_amount} ${primary_mint_symbol}."
+            )
+            is_swapped = asyncio.run(perform_swap(input_amount, primary_mint))
             if is_swapped:
                 df = calc_entry_price(df)
                 df = calc_stoploss(df)
                 df = calc_trailing_stoploss(df)
-                trailing_stoploss = df["trailing_stoploss"].iloc[-1]
                 print(df.tail(2))
-                takeprofit = market().tp = cl.iat[-1] * 1.25
-                market().update_position(True, df["stoploss"].iloc[-1], takeprofit)
-            save_dataframe_to_json(df, json_file_path)
+                takeprofit = cl.iat[-1] * 1.25
+                market_instance.update_position(
+                    True, df["stoploss"].iloc[-1], takeprofit
+                )
+            save_dataframe_to_csv(df, data_file_path)
             return
 
     else:
         # Read DataFrame from JSON file
-        df = read_dataframe_from_json(json_file_path)
+        df = read_dataframe_from_csv(data_file_path)
         print(df.tail(2))
-        input_amount = find_balance(config().secondary_mint)
+        input_amount = find_balance(secondary_mint)
         df = calc_trailing_stoploss(df)
         stoploss = df["stoploss"].iloc[-1]
         trailing_stoploss = df["trailing_stoploss"].iloc[-1]
-        print(stoploss, trailing_stoploss)
 
         # Check Stoploss
         if df["close"].iloc[-1] <= stoploss:
-            sl_msg = f"Soltrade has detected a sell signal for {input_amount} ${config().secondary_mint_symbol}. Stoploss has been reached."
-            log_transaction.info(sl_msg)
-            # log_transaction.info(get_statistics())
-            asyncio.run(perform_swap(input_amount, config().secondary_mint))
+            log_transaction.info(
+                f"Soltrade has detected a sell signal for {input_amount} ${secondary_mint_symbol}. Stoploss has been reached."
+            )
+            asyncio.run(perform_swap(input_amount, secondary_mint))
             stoploss = takeprofit = 0
             df["entry_price"] = None
 
         # Check Trailing Stoploss
-        if trailing_stoploss is not None:
-            if df["close"].iloc[-1] < trailing_stoploss:
-                tsl_msg = f"Soltrade has detected a sell signal for {input_amount} ${config().secondary_mint_symbol}. Trailing stoploss has been reached."
-                log_transaction.info(tsl_msg)
-                # log_transaction.info(get_statistics())
-                asyncio.run(perform_swap(input_amount, config().secondary_mint))
-                stoploss = takeprofit = 0
-                df["entry_price"] = None
-
-        # Check Strategy
-        if df["exit"].iloc[-1] == 1:
-            exit_msg = f"Soltrade has detected a sell signal for {input_amount} ${config().secondary_mint_symbol}."
-            log_transaction.info(exit_msg)
-            # log_transaction.info(get_statistics())
-            asyncio.run(perform_swap(input_amount, config().secondary_mint))
+        elif trailing_stoploss is not None and df["close"].iloc[-1] < trailing_stoploss:
+            log_transaction.info(
+                f"Soltrade has detected a sell signal for {input_amount} ${secondary_mint_symbol}. Trailing stoploss has been reached."
+            )
+            asyncio.run(perform_swap(input_amount, secondary_mint))
             stoploss = takeprofit = 0
             df["entry_price"] = None
-        save_dataframe_to_json(df, json_file_path)
+
+        # Check Strategy
+        elif df["exit"].iloc[-1] == 1:
+            log_transaction.info(
+                f"Soltrade has detected a sell signal for {input_amount} ${secondary_mint_symbol}."
+            )
+            asyncio.run(perform_swap(input_amount, secondary_mint))
+            stoploss = takeprofit = 0
+            df["entry_price"] = None
+
+        save_dataframe_to_csv(df, data_file_path)
 
 
 # This starts the trading function on a timer
@@ -142,15 +140,11 @@ def start_trading():
     perform_analysis()
 
 
-# Function to save DataFrame to JSON file
-def save_dataframe_to_json(df, file_path):
-    df_json = df.to_json(orient="records")
-    with open(file_path, "w") as f:
-        json.dump(df_json, f)
+# Function to save DataFrame to CSV file
+def save_dataframe_to_csv(df, file_path):
+    df.to_csv(file_path, index=False)
 
 
-# Function to read DataFrame from JSON file
-def read_dataframe_from_json(file_path):
-    with open(file_path, "r") as f:
-        df_json = json.load(f)
-    return pd.read_json(df_json)
+# Function to read DataFrame from CSV file
+def read_dataframe_from_csv(file_path):
+    return pd.read_csv(file_path)
